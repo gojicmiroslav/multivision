0e972658a1d216fea138c34cd110fa89ec519d15
Miroslav Gojic <miroslavy2k@gmail.com>
2016-07-05 21:49:16 +0200

    Create main page content

--- server.js
+++ server.js
@@ -22,13 +22,6 @@ db.on('error', console.error.bind(console, 'connection error...'));
 db.once('open', function callback() {
   console.log('multivision db opened');
 });
-var messageSchema = mongoose.Schema({message: String});
-var Message = mongoose.model('Message', messageSchema);
-var mongoMessage;
-Message.findOne().exec(function(err, messageDoc) {
-  mongoMessage = messageDoc.message;
-});
-
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
@@ -46,9 +39,7 @@ app.get('/partials/:partialPath', function(req, res){
 });
 
 app.get('*', function(req, res){
-	res.render('index', {
-		mongoMessage: mongoMessage
-	});
+	res.render('index');
 });
 
 var port = process.env.PORT || 3030;
