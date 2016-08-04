cc69872452239d60e5d1877104ebf60cc41d70ab
Miroslav Gojic <miroslavy2k@gmail.com>
2016-08-04 17:42:04 +0200

    Sorting and filtering

--- public/app/courses/mvCourseListCtrl.js
+++ public/app/courses/mvCourseListCtrl.js
@@ -1,4 +1,9 @@
 angular.module('app').controller('mvCourseListCtrl', function($scope, mvCourse){
-	console.log('mvCourseListCtrl');
 	$scope.courses = mvCourse.query();
+
+	$scope.sortOptions = [
+		{ value: "title", text: "Sort by title" },
+		{ value: "published", text: "Sort by Publish Date" }];
+
+	$scope.sortOrder = $scope.sortOptions[0].value; // set to 'title'
 });
